c29cbf503e72ff856ff7424d6e399644b5c58bb2
Cristian Cornea "DROOPER <59137279+corneacristian@users.noreply.github.com>
2020-03-22 09:57:07 -0400

    Add files via upload

--- decoder.cpp
+++ decoder.cpp
@@ -3,69 +3,169 @@
 using namespace std;
 
 void usb_forensics(char first_hex, char second_hex)
-{
+{ 
 
 
 
 if (first_hex == '0')
 {
-if(second_hex =='4') cout<<"a";
-if(second_hex =='5') cout<<"b";
-if(second_hex =='6') cout<<"c";
-if(second_hex =='7') cout<<"d";
-if(second_hex =='8') cout<<"e";
-if(second_hex =='9') cout<<"f";
-if(second_hex =='a') cout<<"g";
-if(second_hex =='b') cout<<"h";
-if(second_hex =='c') cout<<"i";
-if(second_hex =='d') cout<<"j";
-if(second_hex =='e') cout<<"k";
-if(second_hex =='f') cout<<"l";
+switch (second_hex) {
+  case '4':
+      cout<<"a";
+      break;
+  case '5': 
+      cout<<"b"; 
+      break; 
+  case '6': 
+      cout<<"c"; 
+      break; 
+  case '7': 
+      cout<<"d"; 
+      break; 
+  case '8': 
+      cout<<"e"; 
+      break; 
+  case '9': 
+      cout<<"f"; 
+      break; 
+  case 'a': 
+      cout<<"g"; 
+      break; 
+  case 'b': 
+      cout<<"h"; 
+      break; 
+  case 'c': 
+      cout<<"i"; 
+      break; 
+  case 'j': 
+      cout<<"d"; 
+      break; 
+  case 'e': 
+      cout<<"k"; 
+      break; 
+  case 'f': 
+      cout<<"l"; 
+      break; 
+
+}
 }
 
 
 else if (first_hex == '1')
 {
-if(second_hex =='0') cout<<"m";
-if(second_hex =='1') cout<<"n";
-if(second_hex =='2') cout<<"o";
-if(second_hex =='3') cout<<"p";
-if(second_hex =='4') cout<<"q";
-if(second_hex =='5') cout<<"r";
-if(second_hex =='6') cout<<"s";
-if(second_hex =='7') cout<<"t";
-if(second_hex =='8') cout<<"u";
-if(second_hex =='9') cout<<"v";
-if(second_hex =='a') cout<<"w";
-if(second_hex =='b') cout<<"x";
-if(second_hex =='c') cout<<"y";
-if(second_hex =='d') cout<<"z";
-if(second_hex =='e') cout<<"1";
-if(second_hex =='f') cout<<"2";
+
+switch (second_hex) {
+  case '0':
+      cout<<"m";
+      break;
+  case '1':
+      cout<<"n";
+      break;
+  case '2':
+      cout<<"o";
+      break;
+  case '3':
+      cout<<"p";
+      break;
+  case '4':
+      cout<<"q";
+      break;
+  case '5': 
+      cout<<"r"; 
+      break; 
+  case '6': 
+      cout<<"s"; 
+      break; 
+  case '7': 
+      cout<<"t"; 
+      break; 
+  case '8': 
+      cout<<"u"; 
+      break; 
+  case '9': 
+      cout<<"v"; 
+      break; 
+  case 'a': 
+      cout<<"w"; 
+      break; 
+  case 'b': 
+      cout<<"x"; 
+      break; 
+  case 'c': 
+      cout<<"y"; 
+      break; 
+  case 'e': 
+      cout<<"1"; 
+      break; 
+  case 'f': 
+      cout<<"2"; 
+      break; 
+
+
+}
+
 }
 
 else if (first_hex == '2')
 {
-if(second_hex =='0') cout<<"3";
-if(second_hex =='1') cout<<"4";
-if(second_hex =='2') cout<<"5";
-if(second_hex =='3') cout<<"6";
-if(second_hex =='4') cout<<"7";
-if(second_hex =='5') cout<<"8";
-if(second_hex =='6') cout<<"9";
-if(second_hex =='7') cout<<"0";
-if(second_hex =='c') cout<<" ";
-if(second_hex =='d') cout<<"-";
-if(second_hex =='e') cout<<"=";
-if(second_hex =='f') cout<<"[";
+switch (second_hex) {
+  case '0':
+      cout<<"3";
+      break;
+  case '1':
+      cout<<"4";
+      break;
+  case '2':
+      cout<<"5";
+      break;
+  case '3':
+      cout<<"6";
+      break;
+  case '4':
+      cout<<"7";
+      break;
+  case '5': 
+      cout<<"8"; 
+      break; 
+  case '6': 
+      cout<<"9"; 
+      break; 
+  case '7': 
+      cout<<"0"; 
+      break; 
+  case 'c': 
+      cout<<" "; 
+      break; 
+  case 'd':
+      cout<<"-";
+      break;
+  case 'e': 
+      cout<<"="; 
+      break; 
+  case 'f': 
+      cout<<"["; 
+      break; 
+
+
+}
 }
 
 else if (first_hex == '3')
 {
-if(second_hex =='0') cout<<"]";
-if(second_hex =='2') cout<<"#";
-if(second_hex =='7') cout<<".";
-if(second_hex =='8') cout<<'/';
+switch (second_hex) {
+  case '0':
+      cout<<"]";
+      break;
+  case '2':
+      cout<<"#";
+      break;
+  case '7': 
+      cout<<"."; 
+      break; 
+  case '8': 
+      cout<<'/'; 
+      break;
+}
 }
 
 else if (first_hex == '4') cout<<">";
